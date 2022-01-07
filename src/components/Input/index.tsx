ffc51ac5d9ea490839ebf3f20a62ea6ab471eda6
William Robert <willtaavares@gmail.com>
2022-01-07 14:54:34 -0300

    feat: add Input component structure and styles

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react';
+import { Container } from './styles';
+
+interface InputProps {
+  name: string;
+  title: string;
+  type: string;
+}
+
+function Input({ name, title, type }:InputProps) {
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    if (name === 'user' || 'e-mail') {
+      setErrorMessage('User não encontrado');
+    }
+    if (name === 'senha') {
+      setErrorMessage('senha invalida');
+    }
+  }, [name]);
+
+  return (
+    <Container>
+      <p>{title}</p>
+      <input
+        type={type}
+        onBlur={() => setError(true)}
+      />
+      {error && <span>{errorMessage}</span>}
+    </Container>
+  );
+}
+
+export default Input;
