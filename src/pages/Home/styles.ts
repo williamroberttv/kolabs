e045ba2008544f55a63d462c229dc73a1c68af17
William Robert <willtaavares@gmail.com>
2022-01-07 21:14:12 -0300

    feat: add homepage background particules and styles

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,32 @@
 import styled from 'styled-components';
+import Particles from 'react-tsparticles';
 
-export const Container = styled.div`
+interface TextureProps{
+  imageUrl: string;
+}
 
+export const Container = styled(Particles)`
+width: 100%;
+height: 100vh;
+position: absolute;
+background: linear-gradient(60deg, rgba(255,255,255,1) 28%,
+ rgba(227,255,254,1) 42%, rgba(197,145,255,1) 100%);
+ z-index:-1;
+`;
+
+export const Texture = styled.div<TextureProps>`
+  background: url(${props => props.imageUrl}) repeat;
+  opacity: 0.3;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Content = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
 `;
